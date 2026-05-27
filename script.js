801dd63b360c5743413b109428a98c1c47a1a4ca
Learning <snehadani635@gmail.com>
2026-05-27 21:31:04 +0530

    Updated UI of login system

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function showRegister(){
-    document.getElementById("loginForm").style.display="none";
     document.getElementById("registerForm").style.display="block";
 }
 
 function showLogin(){
-    document.getElementById("registerForm").style.display="none";
     document.getElementById("loginForm").style.display="block";
 }
 
